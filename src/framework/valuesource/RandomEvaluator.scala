7c449e7e61646a8baaee3dc8e252b4a99ae2173a
Andrew Miller <ak.miller@auckland.ac.nz>
2011-11-15 15:05:46 +1300

    Add RandomEvaluator.scala

---
+++ src/framework/valuesource/RandomEvaluator.scala
@@ -0,0 +1,46 @@
+package framework
+
+import fieldml.evaluator._
+import fieldml.valueType._
+import scala.util.Random
+
+import framework.valuesource._
+
+import value._
+
+class RandomEvaluator( val randomArgument : ArgumentEvaluator, valueType : EnsembleType )
+    extends Evaluator( "random.0d.equiprobable", valueType )
+    with ValueSource
+{
+    def variables = List()
+    
+    var cache : Option[Value] = None
+
+    def isEventuallyUnbound( state : EvaluationState, arg : ArgumentEvaluator ) : Boolean =
+    {
+      val bind = state.getBind( arg )
+      bind match {
+        case Some(a : ArgumentEvaluator) => isEventuallyUnbound(state, a)
+        case Some(_) => false
+        case None => true
+      }
+    }
+
+    def evaluate( state : EvaluationState ) : Option[Value] =
+    {
+      cache match {
+        case None => {
+          cache = if (isEventuallyUnbound( state, randomArgument )) {
+            val number = math.abs(Random.nextInt()) % valueType.elementCount
+            println( "EnsembleValue.apply: " + number )
+            Some(EnsembleValue.apply( valueType,  number ))
+          } else {
+            println("Argument appears bound; random tag is identity function.");
+            randomArgument.evaluate( state )
+          }
+          cache
+        }
+        case v@(Some(_)) => v
+      }
+    }
+}
